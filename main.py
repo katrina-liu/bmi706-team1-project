412c0d03c9feaf5eb1b41198341edddb71a287ad
qiuqiu <tian_jin@hms.harvard.edu>
2023-03-01 18:48:32 -0500

    bar chart of number of records

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import streamlit as st
+import altair as alt
+import pandas as pd
 
 st.title("CIVic Visualization Tool")
 
-# TODO: Time series chart
+# load data 
+df = pd.read_csv("data/civic_data.tsv")
+df_unique = pd.read_csv("data/civic_data_unique.tsv")
+
+### Number of Records of All Diseases Reported in a Year ###
+
+# replace with st.slider
+Year = st.slider("Year", min(df["year"]), max(df["year"]), value = 2018)
+subset = df[df["year"] == Year]
+
+plot1 = alt.Chart(subset).mark_bar().encode(
+    x=alt.X('disease:N', title="Diseases", sort="-y"),
+    y=alt.Y(aggregate = "count", type='quantitative', title="Number of Records")
+).properties(
+    title=f"Number of Records of All Diseases Reported in {Year}",
+    width=600,
+    height=600
+)
+
+st.altair_chart(plot1)
