70725fe88bd55360cd679b665179f1838bf460c5
qiuqiu <tian_jin@hms.harvard.edu>
2023-03-01 21:02:15 -0500

    jjj

--- pages/variant.py
+++ pages/variant.py
@@ -2,19 +2,43 @@ import streamlit as st
 
 df = pd.read_csv("data/civic_data.tsv")
 df_unique = pd.read_csv("data/civic_data_unique.tsv")
+df_unique.dropna(subset=['drugs','variant','disease'])
 
 variant = st.text_input("Search a variant here:")
 
-if len(variant) > 0:  # TODO: Change this to if variant is valid
+if len(variant) > 0 and variant in df_unique["variant"].unique():  # TODO: Change this to if variant is valid
     disease_tab, therapy_tab = st.tabs(["Disease", "Therapy"])
+    df_unique_variant = df_unique[df_unique["variant"] == variant]
+    
     with disease_tab:
         st.header("Number of Evidences Showing Connection between " + variant +
                   " and Diseases")
-        # TODO: add donut
+        donut_v_d = alt.Chart(df_unique_variant).mark_arc(innerRadius=50, outerRadius=90).encode(
+            theta = alt.Theta("num_ev:Q"),
+            color = alt.Color("disease:N", title = "Diseases"),
+            tooltip=["num_ev:Q", "disease:N","variant:N"]
+            ).transform_aggregate(
+                num_ev='count(evidence_id)',
+                groupby=["disease","variant"]
+                ).properties(
+                    width=250
+                    )
+        st.altair_chart(donut_v_d)
+        
     with therapy_tab:
         st.header("Number of Evidences Showing Connection between " + variant +
                   " and Therapies")
-        # TODO: add donut
+        donut_v_t = alt.Chart(df_unique_variant).mark_arc(innerRadius=50, outerRadius=90).encode(
+            theta = alt.Theta("num_ev:Q"),
+            color = alt.Color("drugs:N", title = "Therapies"),
+            tooltip=["num_ev:Q", "variant:N","drugs:N"]
+            ).transform_aggregate(
+                num_ev='count(evidence_id)',
+                groupby=["variant","drugs"]
+                ).properties(
+                    width=250
+                    )
+        st.altair_chart(donut_v_t)
 else:
     st.title("Variant")
     # TODO: maybe show some overview charts about Variants
